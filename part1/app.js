934d355b4494617b0fe796b677db22a1d9b15b90
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 07:25:21 +0000

    [automatic commit] 2025-06-20 07:25:21 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -35,17 +35,11 @@ let db;
     });
 
     // Create a table if it doesn't exist
-    await db.execute(`
-      
+    await db.execute(` 
+
     `);
 
-    CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    role ENUM('owner', 'walker') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 );
 
 CREATE TABLE Dogs (
