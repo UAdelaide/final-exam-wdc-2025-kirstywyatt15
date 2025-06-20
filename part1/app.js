97c27145763420aee1c77aa7e650c5e086894513
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 07:32:14 +0000

    [automatic commit] 2025-06-20 07:32:14 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -101,7 +101,9 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
 
-        INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        
 
 INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
 
