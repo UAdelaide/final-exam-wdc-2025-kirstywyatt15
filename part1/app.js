db7bbbd6bdd98b0082e3cdf0affdfcd4ed50b827
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 09:02:39 +0000

    [automatic commit] 2025-06-20 09:02:39 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -205,7 +205,7 @@ app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [openRequests] = await db.execute(`
         SELECT w.username AS walker_username,
-        rat.rating AS total rating 
+        rat.rating AS total_rating <---- needs a set 
         o.username AS owner_username
         FROM WalkRequests r
         JOIN Dogs d ON r.dog_id = d.dog_id
