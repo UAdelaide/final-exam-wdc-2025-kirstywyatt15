acd8800d86f9083496570d6666762631a02d0bee
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 07:26:39 +0000

    [automatic commit] 2025-06-20 07:26:39 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -35,7 +35,7 @@ let db;
     });
 
     // Create a Users table if it doesn't exist
-    await db.execute(`CREATE TABLE Users (
+    await db.execute(`CREATE TABLE IF NOT EXISTS Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
