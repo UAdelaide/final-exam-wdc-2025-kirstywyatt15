ae0e6c752059149e31f9510acbd6b1b06872a4e0
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 21:24:56 +0000

    [automatic commit] 2025-06-20 21:24:56 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -218,7 +218,7 @@ app.get('/api/walkers/summary', async (req, res) => {
         AND WHERE average_rating = (SELECT AVG(rating)
         FROM WalkRatings
         WHERE )
-        AND WHERE completed_walks = (SELECT status from)
+        AND WHERE completed_walks = (SELECT status FROM )
         `);
     res.json(walkers);
   } catch (err) {
