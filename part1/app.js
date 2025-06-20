d58ef76505297254268eeb0532f8771a01959bcb
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 08:19:53 +0000

    [automatic commit] 2025-06-20 08:19:53 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -160,7 +160,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [openRequests] = await db.execute(`SELECT r.request_id, d.name, r.requested_time, r.duration_minutes, r.location
-        WHERE r.status = open
+        WHERE r.status = open or 1??
          SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 1 FROM Dog
 s WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max');`);
     res.json(openRequests);
