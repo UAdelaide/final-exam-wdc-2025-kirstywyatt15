2f0dcef390f404c5751f58adf6ba13065b2762ec
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 21:43:31 +0000

    [automatic commit] 2025-06-20 21:43:31 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -221,7 +221,8 @@ app.get('/api/walkers/summary', async (req, res) => {
         AND WHERE completed_walks = (SELECT COUNT(status)
         FROM WalkRequests r
         JOIN WalkRatings w ON r.request_id = w.request_id
-        HAVING)
+        GROUP BY status
+        HAVING )
         GROUP BY o.username
          ----- CHECK: ratings for WHERE walkrequest status = completed or will likely mess up avderage / totals -----
         `);
