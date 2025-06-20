ff95ed1fad0dd6a7c71639920d094f3a91998908
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 22:19:13 +0000

    [automatic commit] 2025-06-20 22:19:13 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -114,8 +114,10 @@ let db;
     if (dogs_rows[0].count === 0) {
       await db.execute(`
 INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Max', 'medium' FROM Users WHERE user_id = (SELECT user_id FROM Users WHERE username = 'alice123');
-INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Bella', 'small' FROM Users WHERE user_id = (SELECT user_id FROM Users WHERE username = 'carol1
+
+await db.execute(`INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Bella', 'small' FROM Users WHERE user_id = (SELECT user_id FROM Users WHERE username = 'carol1
 23')`);
+
 INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Pickles', 'small' FROM Users WHERE user_id = (SELECT user_id FROM Users WHERE username = 'caro
 l123');
 INSERT INTO Dogs (owner_id, name, size) SELECT user_id, 'Muffin', 'medium' FROM Users WHERE user_id = (SELECT user_id FROM Users WHERE username = 'fran
