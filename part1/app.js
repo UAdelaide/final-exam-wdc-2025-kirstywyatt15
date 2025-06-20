9792f3bed0929c85ac943b3a8f6c552f507b29fd
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 08:44:18 +0000

    [automatic commit] 2025-06-20 08:44:18 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -140,6 +140,11 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 s WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Pickles');
 `);
 
+// Insert data if WalkRequests table is empty
+    const [WR_rows] = await db.execute('SELECT COUNT(*) AS count FROM WalkRequests');
+    if (WR_rows[0].count === 0) {
+      await db.execute(`
+
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
