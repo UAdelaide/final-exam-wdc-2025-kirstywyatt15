51a14ad7f9e10848c39f9bac59991347e97151b3
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-20 22:48:03 +0000

    [automatic commit] 2025-06-20 22:48:03 UTC 220.253.83.98

--- part1/app.js
+++ part1/app.js
@@ -236,7 +236,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 });
 
 // Return summary of each walker with their average rating and number of completed walks
-// edited so that only ratings for 
+// edited so that only ratings for completed walks show
 app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [walkers] = await db.execute(`
