9fb98ade5e9be7f426ab47beb52ed5e9d4f3708d
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 01:29:52 +0000

    [automatic commit] 2025-06-21 01:29:52 UTC 220.253.83.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -82,7 +82,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', async (req, res) => {
 
   req.session.destroy(err) => {
-    if (err) 
+    if (err) return res.status(500).send
   }
 
 });
