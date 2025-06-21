0c4525cb3610cf728806fd7fe5243108f2bc9f65
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 00:20:27 +0000

    [automatic commit] 2025-06-21 00:20:27 UTC 220.253.83.98

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-
+//added this so
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
