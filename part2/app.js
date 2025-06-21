54ef036329dbf5bfe741d97afbfa87754eb8dcfe
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 00:40:42 +0000

    [automatic commit] 2025-06-21 00:40:42 UTC 220.253.83.98

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-// added this so my html forms can be read by my routers
+// added this so my html forms can be read by my routers (parses into a JS object)
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
