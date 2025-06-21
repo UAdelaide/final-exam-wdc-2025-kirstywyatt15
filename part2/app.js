531ae756ce84228f7f8fcb4eaafeb773d545afc5
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 00:55:46 +0000

    [automatic commit] 2025-06-21 00:55:46 UTC 220.253.83.98

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            maxAge: 1 * 60 * 60 * 1000
+            maxAge: 1 * 60 * 60 * 1000 // 1 hr
         }
         })
 );
