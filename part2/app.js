8ae56987befd3eec6c3dc7db6eeb1405f14cad03
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 01:14:53 +0000

    [automatic commit] 2025-06-21 01:14:53 UTC 220.253.83.98

--- part2/app.js
+++ part2/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-// added session and cookies
+// added session
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
@@ -24,8 +24,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 // added this so my html forms can be read by my routers (parses into a JS object)
 app.use(express.urlencoded({ extended: false }));
-// aded this so my app can use cookies
-app.use(cookieParser());
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
