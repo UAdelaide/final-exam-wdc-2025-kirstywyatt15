4dc6b050873f7b98168f51bc2da4212972759834
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 02:49:20 +0000

    [automatic commit] 2025-06-21 02:49:20 UTC 220.253.83.98

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+// GET owners Dogs
+router.get()
+
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
   try {
