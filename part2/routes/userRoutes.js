24bb7186051d692930c9d4fc9f975093c5a11ca9
Kirsty Wyatt <kirsty.wyatt@student.adelaide.edu.au>
2025-06-21 00:21:26 +0000

    [automatic commit] 2025-06-21 00:21:26 UTC 220.253.83.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,8 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    console.log('login successful')
+    console.log('login successful');
+    
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
